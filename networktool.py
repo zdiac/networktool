e3b2b316f4ed4accd9d8708988ba2a5bc2658c5e
Elias <endzish@gmail.com>
2026-04-13 13:26:37 +0200

    fix: ändrar koden för jag hade två main

--- networktool.py
+++ networktool.py
@@ -1,15 +1,20 @@
+logg = []  # Lista som ska lagra loggmeddelanden
+
 def main():
-    print("Välkommen till NetworkTool (Python-version)")
-    print("Meny kommer här senare...")
+    while True:
+        print("\n--- NetworkTool ---")
+        print("1. Validera IP-adress")
+        print("2. Validera portnummer")
+        print("3. Visa logg")
+        print("4. Avsluta")
+
+        choice = input("Välj ett alternativ: ")
+
+        if choice == "4":
+            print("Avslutar programmet...")
+            break
+        else:
+            print("Funktionen är inte implementerad ännu.")
 
 if __name__ == "__main__":
     main()
-
-logg = []  # Lista som ska lagra loggmeddelanden
-def main():
-    while True:
-        printf("\n--- NetworkTool ---")
-        printf("1. Validera IP-adress")
-        printf("2. Validera portnummer")
-        printf("3. Visa logg")
-        printf("4. Avsluta")
