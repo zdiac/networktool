e5299e293910fb1381dd3170548ef005b2ca44b1
Elias <endzish@gmail.com>
2026-04-13 13:13:49 +0200

    feat: lägger till första delen av koden

--- networktool.py
+++ networktool.py
@@ -4,3 +4,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+logg = []  # Lista som ska lagra loggmeddelanden
+def main():
+    while True:
+        printf("\n--- NetworkTool ---")
+        printf("1. Validera IP-adress")
+        printf("2. Validera portnummer")
+        printf("3. Visa logg")
+        printf("4. Avsluta")
