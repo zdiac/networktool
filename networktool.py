762f3df8f8dad1871997fd3b12dd2b2f2ab2b0e7
Elias <endzish@gmail.com>
2026-04-14 10:42:53 +0200

    fix: rättat felaktig kod, programmet klart.

--- networktool.py
+++ networktool.py
@@ -17,7 +17,7 @@ def validate_ip(ip):
             return
         
         nummer = int(del_)
-        if nummer < 0 or  nummer > 255:
+        if nummer < 0 or nummer > 255:
             print("Ogiltig IP-adress: varje del måste vara 0-255.")
             return
         
@@ -26,9 +26,9 @@ def validate_ip(ip):
 
 def validate_port(port):
 
-    logg.apend(f"[{datetime.now()}] Validerade port: {port}")
+    logg.append(f"[{datetime.now()}] Validerade port: {port}")
 
-    if not port.isdigit()
+    if not port.isdigit():
         print("Ogiltigt portnummer: måste vara en siffra")
         return
     
@@ -55,24 +55,22 @@ def main():
         if choice == "1":
             ip = input("Ange en IP-adress: ")
             validate_ip(ip)
-            logg.append(f"Validerade IP: {ip}")
                 
         elif choice == "2":
             port = input("Ange ett portnummer: ")
             validate_port(port)  
-            logg.append(f"Validerade port: {port}") 
 
         elif choice == "3":
             print("\n--- Logg ----")
-            for entry in logg:
-                print(entry)    
+            for rad in logg:
+                print(rad)    
 
         elif choice =="4":
             print("Avslutar programmet...")
             break
 
         else:
-            print("Funktionen är inte implementerad ännu.")
+            print("Ogiltigt val. Välj mellan 1 och 4.")
 
 
 if __name__ == "__main__":
